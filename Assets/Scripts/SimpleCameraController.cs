9f0969bc3c0e1b15ae2fb9abd10625b907b497a1
tonymtz <atony.mtz@gmail.com>
2023-10-07 14:02:25 -0700

    fixes corner tiles in cave style platforms

--- Assets/Scripts/SimpleCameraController.cs
+++ Assets/Scripts/SimpleCameraController.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 
 public class SimpleCameraController : MonoBehaviour {
-	Vector3 _offset = new Vector3(0f, 0f, 0f);
 	Vector3 _velocity = Vector3.zero;
 
 	[SerializeField] Transform target;
 	[SerializeField] float smoothTime = 0.1f;
+	[SerializeField] Vector3 offset;
 
 	void LateUpdate () {
-		Vector3 targetPosition = new Vector3(target.position.x, transform.position.y, transform.position.z) + _offset;
+		Vector3 targetPosition = new Vector3(target.position.x, transform.position.y, transform.position.z) + offset;
 		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, smoothTime);
 	}
 }
